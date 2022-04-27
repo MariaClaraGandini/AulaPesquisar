418bcc539d78c48b0eb6221e07ba5b7ec2c3acb5
MariaClaraGandini <maria.pereira53@fatec.sp.gov.br>
2022-04-27 10:56:37 -0300

    Add Banco de dados

---
+++ AulaPesquisar/Entidades/Permissao_Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaPesquisar.Entidades
+{
+    public class Permissao_Usuario
+    {
+        public int Id { get; set; }
+        public string UsuarioId { get; set; }
+        public string PermissaoId { get; set; }
+        public  Permissao Permissao { get; set; }
+        public  Usuarios Usuario { get; set; }
+
+        //Permissao permissao -ligação virtual 
+    }
+}
